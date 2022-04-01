0aa3db017a2056f762d6b9fa189e2e6cf498ccb4
Kelsi Wood <kwood1@mines.edu>
2022-04-01 17:05:08 -0600

    built half of create suggestions for computer player

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,6 +1,8 @@
 package clueGame;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ComputerPlayer extends Player {
@@ -11,6 +13,26 @@ public class ComputerPlayer extends Player {
 	}
 	
 	public Solution createSuggestion(CardCollection deck, Card roomCard) {
+		var personCards = deck.getPersonCards();
+		var weaponCards = deck.getWeaponCards();
+		
+		var seenPlayers = this.getSeen().getPersonCards();
+		var seenWeapons = this.getSeen().getWeaponCards();
+		
+		//players
+		var unseenPlayers = new ArrayList<Card>();
+		unseenPlayers.addAll(personCards);
+		unseenPlayers.removeAll(seenPlayers);
+		
+		//weapons
+		var unseenWeapons = new ArrayList<Card>();
+		unseenWeapons.addAll(weaponCards);
+		unseenWeapons.removeAll(seenWeapons);
+		
+		//make rand - get rand
+		
+		
+		
 		return null;
 	}
 	
